4918c318eb9c41918615f29a4f56ae875e423e4b
Hendry Tanaka <tanaka@pondokcoder.com>
2020-06-28 17:25:30 +0700

    Modul Master INV Kategori

--- client/script/master/inventori/kategori/index.php
+++ client/script/master/inventori/kategori/index.php
@@ -1,5 +1,6 @@
 <script type="text/javascript">
 	$(function(){
+		var MODE = "tambah", selectedUID;
 		var tableKategori = $("#table-kategori").DataTable({
 			"ajax":{
 				url: __HOSTAPI__ + "/Inventori/kategori",
@@ -24,16 +25,16 @@
 				},
 				{
 					"data" : null, render: function(data, type, row, meta) {
-						return row["nama"];
+						return "<span id=\"nama_" + row["uid"] + "\">" + row["nama"] + "</span>";
 					}
 				},
 				{
 					"data" : null, render: function(data, type, row, meta) {
 						return "<div class=\"btn-group\" role=\"group\" aria-label=\"Basic example\">" +
-									"<a href=\"" + __HOSTNAME__ + "/master/kendaraan/jenis/edit/" + row["uid"] + "\" class=\"btn btn-info btn-sm\">" +
+									"<button class=\"btn btn-info btn-sm btn-edit-kategori\" id=\"kategori_edit_" + row["uid"] + "\">" +
 										"<i class=\"fa fa-pencil\"></i> Edit" +
-									"</a>" +
-									"<button id=\"delete_" + row['uid'] + "\" class=\"btn btn-danger btn-sm btn-delete-jenis\">" +
+									"</button>" +
+									"<button id=\"kategori_delete_" + row['uid'] + "\" class=\"btn btn-danger btn-sm btn-delete-kategori\">" +
 										"<i class=\"fa fa-trash\"></i> Hapus" +
 									"</button>" +
 								"</div>";
@@ -42,23 +43,104 @@
 			]
 		});
 
-		$("body").on("click", ".btn-delete-jenis", function(){
-			var id = $(this).attr("id").split("_");
-			id = id[id.length - 1];
+		$("body").on("click", ".btn-delete-kategori", function(){
+			var uid = $(this).attr("id").split("_");
+			uid = uid[uid.length - 1];
 
-			var conf = confirm("Hapus jenis kendaraan?");
+			var conf = confirm("Hapus kategori item?");
 			if(conf) {
 				$.ajax({
-					url:__HOSTAPI__ + "/Kendaraan/kendaraan_jenis/" + id,
+					url:__HOSTAPI__ + "/Inventori/master_inv_kategori/" + uid,
 					beforeSend: function(request) {
 						request.setRequestHeader("Authorization", "Bearer " + <?php echo json_encode($_SESSION["token"]); ?>);
 					},
 					type:"DELETE",
-					success:function(resp) {
-						tableJenis.ajax.reload();
+					success:function(response) {
+						tableKategori.ajax.reload();
+					},
+					error: function(response) {
+						console.log(response);
 					}
 				});
 			}
 		});
+
+		$("body").on("click", ".btn-edit-kategori", function() {
+			var uid = $(this).attr("id").split("_");
+			uid = uid[uid.length - 1];
+			selectedUID = uid;
+			MODE = "edit";
+			$("#txt_nama").val($("#nama_" + uid).html());
+			$("#form-tambah").modal("show");
+			return false;
+		});
+
+		$("#tambah-kategori").click(function() {
+
+			$("#form-tambah").modal("show");
+			MODE = "tambah";
+
+		});
+
+		$("#btnSubmit").click(function() {
+			var nama = $("#txt_nama").val();
+			if(nama != "") {
+				var form_data = {};
+				if(MODE == "tambah") {
+					form_data = {
+						"request": "tambah_kategori",
+						"nama": nama
+					};
+				} else {
+					form_data = {
+						"request": "edit_kategori",
+						"uid": selectedUID,
+						"nama": nama
+					};
+				}
+
+				$.ajax({
+					async: false,
+					url: __HOSTAPI__ + "/Inventori",
+					data: form_data,
+					beforeSend: function(request) {
+						request.setRequestHeader("Authorization", "Bearer " + <?php echo json_encode($_SESSION["token"]); ?>);
+					},
+					type: "POST",
+					success: function(response){
+						$("#txt_nama").val("");
+						$("#form-tambah").modal("hide");
+						tableKategori.ajax.reload();
+					},
+					error: function(response) {
+						console.log(response);
+					}
+				});
+			}
+		});
+
 	});
 </script>
+
+<div id="form-tambah" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-large-title" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+	<div class="modal-dialog modal-md bg-danger" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="modal-large-title">Tambah Kategori</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="form-group col-md-12">
+					<label for="txt_no_skp">Nama Kategori:</label>
+					<input type="text" class="form-control" id="txt_nama" />
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Kembali</button>
+				<button type="button" class="btn btn-primary" id="btnSubmit">Submit</button>
+			</div>
+		</div>
+	</div>
+</div>
