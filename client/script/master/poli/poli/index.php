54d6ea26fb5151af45fc47aeb3612137d5e68f2f
wakandaBP <byte007@zoho.com>
2020-07-03 01:57:19 +0700

    Edit Poli dan Tindakan Poli 1. Edit poli dan tindakan poli selesai 2. Perbaikan kode file script di tambah poli 3. View Detail Tindakan Poli pada index akan di hold sementara

--- client/script/master/poli/poli/index.php
+++ client/script/master/poli/poli/index.php
@@ -1,5 +1,7 @@
 <script type="text/javascript">
 	$(function(){
+		var penjamin = loadPenjamin();
+
 		var MODE = "tambah", selectedUID;
 		var tablePoli = $("#table-poli").DataTable({
 			"ajax":{
@@ -31,9 +33,9 @@
 				{
 					"data" : null, render: function(data, type, row, meta) {
 						return "<div class=\"btn-group\" role=\"group\" aria-label=\"Basic example\">" +
-									"<button id=\"poli_view_" + row['uid'] + "\" class=\"btn btn-warning btn-sm btn-detail-poli\">" +
+									/*"<button id=\"poli_view_" + row['uid'] + "\" class=\"btn btn-warning btn-sm btn-detail-poli\">" +
 									 	"<i class=\"fa fa-list\"></i> Detail" +
-									"</button>" +
+									"</button>" +*/
 									"<a href=\"" + __HOSTNAME__ + "/master/poli/poli/edit/" + row["uid"] + "\" class=\"btn btn-info btn-sm btn-edit-poli\">" +
 										"<i class=\"fa fa-edit\"></i> Edit" +
 									"</a>" +
@@ -68,5 +70,113 @@
 			}
 		});
 
+
+
+		$("#table-poli tbody").on('click', '.btn-detail-poli', function(){
+            //==== Set table head
+            var html = "<tr>" +
+                        "<th style='width: 10px;'>No</th>" +
+                        "<th>Tindakan</th>";
+
+
+            $.each(penjamin, function(key, item){
+                html += "<th class='col_"+ item.uid +"'>"+ item.nama +"</th>";
+            });
+
+            html += "</tr>";
+
+            $("#table-view-tindakan thead").html(html);
+            //====
+
+            //==== Set table content
+            var no = 1;
+            var html_content = ""; 
+            $.each(tindakan, function(key, item){
+
+                if (item.uid in hargaPenjamin){
+                    html_content += "<tr>" + 
+                                    "<td>"+ no +"</td>" +
+                                    "<td>"+ item.nama +"</td>";
+
+                    var parent_uid = item.uid;
+
+                    $.each(penjamin, function(key, item){
+                        if (item.uid in hargaPenjamin[parent_uid]){
+                            html_content += "<td><span class='separated_comma'>Rp. "+ hargaPenjamin[parent_uid][item.uid] +"</span></td>";
+                        } else {
+                            html_content += "<td> - </td>";
+                        }
+                    });
+
+                    no++;
+                }
+                html_content += "</tr>";
+            });
+
+            $("#table-konfirmasi tbody").html(html_content);
+            $(".separated_comma").digits();
+            //====
+
+            
+            $("#title-konfirmasi-poli").html(dataObject.nama);
+            
+        });
 	});
+	
+
+	/*========== FUNC FOR LOAD PENJAMIN ==========*/
+    function loadPenjamin(){
+        var dataPenjamin;
+
+        $.ajax({
+            async: false,
+            url:__HOSTAPI__ + "/Penjamin/penjamin",
+            type: "GET",
+             beforeSend: function(request) {
+                request.setRequestHeader("Authorization", "Bearer " + <?php echo json_encode($_SESSION["token"]); ?>);
+            },
+            success: function(response){
+                var MetaData = dataPenjamin = response.response_package.response_data;
+            },
+            error: function(response) {
+                console.log(response);
+            }
+        });
+
+
+        if (dataPenjamin.length > 0) {
+            return dataPenjamin;
+        } else {
+            return null;
+        }
+    }
+    /*--------------------------------------*/
 </script>
+
+
+<div id="view-detail" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="modal-large-title" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+	<div class="modal-dialog modal-md bg-danger" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="modal-large-title">Tindakan dari : <span id="title-tindakan"></span></h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<table class="table table-bordered" id="table-view-tindakan">
+					<thead>
+						
+					</thead>
+
+					<tbody>
+						
+					</tbody>
+				</table>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Kembali</button>
+			</div>
+		</div>
+	</div>
+</div>
