d0069e3ac3879ac45af87f04b9667e70b7407c68
Hendry Tanaka <tanaka@pondokcoder.com>
2020-07-08 21:50:31 +0700

    Init PO

--- client/pages/inventori/po/index.php
+++ client/pages/inventori/po/index.php
@@ -4,31 +4,29 @@
 			<nav aria-label="breadcrumb">
 				<ol class="breadcrumb mb-0">
 					<li class="breadcrumb-item"><a href="<?php echo __HOSTNAME__; ?>/">Home</a></li>
-					<li class="breadcrumb-item"><a href="<?php echo __HOSTNAME__; ?>/master/inventori">Master Inventori</a></li>
-					<li class="breadcrumb-item active" aria-current="page">Satuan</li>
+					<li class="breadcrumb-item"><a href="<?php echo __HOSTNAME__; ?>/inventori">Inventori</a></li>
+					<li class="breadcrumb-item active" aria-current="page">Purchase Order</li>
 				</ol>
 			</nav>
+			<h4>Inventori - PO</h4>
 		</div>
-		<button class="btn btn-sm btn-info" id="tambah-kunjungan">
+		<a href="<?php echo __HOSTNAME__; ?>/inventori/po/tambah" class="btn btn-sm btn-info" id="tambah-gudang">
 			<i class="fa fa-plus"></i> Tambah
-		</button>
+		</a>
 	</div>
 </div>
+
+
 <div class="container-fluid page__container">
 	<div class="row card-group-row">
 		<div class="col-lg-12 col-md-12 card-group-row__col">
 			<div class="card card-group-row__card card-body card-body-x-lg flex-row align-items-center">
-				<table class="table table-bordered" id="table-kunjungan">
+				<table class="table table-bordered" id="table-gudang">
 					<thead>
 						<tr>
 							<th style="width: 20px;">No</th>
-							<th>Waktu</th>
-							<th>Kategori</th>
-							<th>No. RM</th>
-							<th>Nama</th>
-							<th>Dokter</th>
-							<th>Keterangan</th>
-							<th>Oleh</th>
+							<th>Supplier</th>
+							<th>Pegawai</th>
 							<th>Aksi</th>
 						</tr>
 					</thead>
