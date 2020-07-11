0b40cd700f6db84af091b4e9dd0be26a3d96dd96
wakandaBP <byte007@zoho.com>
2020-07-11 13:50:35 +0700

    Add Assesmen awal, undone. It will be finish after applicares

---
+++ client/pages/rawat_jalan/perawat/halaman1.php
@@ -0,0 +1,409 @@
+<div class="row">
+	<div class="col-lg">
+		<div class="card">
+			<div class="card-header bg-white d-flex align-items-center">
+				<h5 class="card-header__title flex m-0"><i class="material-icons mr-3">info_outline</i> Informasi Pasien</h5>
+			</div>
+			<div class="card-body ">
+				<div class="col-md-12">
+					<table class="table">
+						<tbody>
+							<tr>
+								<td>No. Rekam Medis</td>
+								<td> : </td>
+								<td><span id="no_rm"><b>11-11-11</b></span></td>
+								<td>Tanggal Lahir</td>
+								<td> : </td>
+								<td><span id="tanggal_lahir"><b>22 Januari 1998</b></span></td>
+							</tr>
+							<tr>
+								<td>Nama Pasien</td>
+								<td> : </td>
+								<td><span id="panggilan"><b>Tn.</b></span> <span id="nama"><b>John Doe</b></span></td>
+								<td>Jenis Kelamin</td>
+								<td> : </td>
+								<td><span id="jenkel"><b>Laki-laki</b></span></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg">
+		<div class="card">
+			<div class="card-header d-flex align-items-center">
+				<h5 class="card-header__title flex m-0">Informasi Pendaftaran</h5>
+			</div>
+			<div class="card-body ">
+				<div class="row">
+					<div class="col-md-6 row form-group">
+						<div class="col-md-4">
+							<label>Pendaftaran</label>
+						</div>
+						<div class="col-md-8">
+							<input type="" name="" id="waktu_masuk" disabled class="form-control" value="12 Juli 2020 10:31:36">
+						</div>
+					</div>
+					<div class="col-md-6 row form-group">
+						<div class="col-md-4">
+							<label>Cara Pembayaran</label>
+						</div>
+						<div class="col-md-8">
+							<input type="" name="" id="penjamin" disabled class="form-control" value="BPJS KESEHATAN">
+						</div>
+					</div>
+					<div class="col-md-6 row form-group">
+						<div class="col-md-4">
+							<label>Penanggung Jawab Pasien</label>
+						</div>
+						<div class="col-md-8">
+							<input type="" name="" id="pj_pasien" class="form-control" value="">
+						</div>
+					</div>
+					<div class="col-md-6 row form-group">
+						<div class="col-md-4">
+							<label>Informasi di Dapat Dari</label>
+						</div>
+						<div class="col-md-8">
+							<input type="" name="" id="pj_pasien"  class="form-control" value="">
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row" style="margin-top: 20px;">
+	<div class="col-md-8">
+		<div class="card">
+			<div class="card-header d-flex align-items-center">
+				<h5 class="card-header__title flex m-0">Pengkajian Keperawatan</h5>
+			</div>
+			<div class="card-body">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="row">
+							<div class="form-group col-lg-12">
+								 <div class="col-12 col-md-12 mb-3">
+									<label for="kesadaran">Kesadaran :</label>
+									<div class="row col-md-12" id="parent_jenkel">
+										<div class="col-md-6">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="kesadaran" value="kesadaran_1" id="kesadaran_1">
+												<label class='custom-control-label' for="kesadaran_1">Compos Mentis</label>
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="kesadaran" value="kesadaran_2" id="kesadaran_2">
+												<label class='custom-control-label' for="kesadaran_2">Apatis</label>
+											</div>
+										</div>
+									</div>
+			                    </div>
+							</div>
+							<div class="form-group col-lg-12">
+								 <div class="col-12 col-md-12 mb-3">
+									<label for="sikap_tubuh">Sikap Tubuh :</label>
+									<div class="row col-md-12" id="sikap_tubuh">
+										<div class="col-md-2">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="sikap_tubuh" value="sikap_tubuh_1" id="sikap_tubuh_1">
+												<label class='custom-control-label' for="sikap_tubuh_1">Normal</label>
+											</div>
+										</div>
+										&nbsp; &nbsp;&nbsp;&nbsp;
+										<div class="col-md-2">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="sikap_tubuh" value="sikap_tubuh_2" id="sikap_tubuh_2">
+												<label class='custom-control-label' for="sikap_tubuh_2">Lordosis</label>
+											</div>
+										</div>
+										&nbsp; &nbsp;&nbsp;&nbsp;
+										<div class="col-md-2">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="sikap_tubuh" value="sikap_tubuh_3" id="sikap_tubuh_3">
+												<label class='custom-control-label' for="sikap_tubuh_3">Kifosis</label>
+											</div>
+										</div>
+										&nbsp; &nbsp;&nbsp;&nbsp;
+										<div class="col-md-2">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="sikap_tubuh" value="sikap_tubuh_4" id="sikap_tubuh_4">
+												<label class='custom-control-label' for="sikap_tubuh_4">Skoliosis</label>
+											</div>
+										</div>
+										&nbsp; &nbsp;&nbsp;&nbsp;
+										<div class="col-md-2">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="sikap_tubuh" value="sikap_tubuh_5" id="sikap_tubuh_5">
+												<label class='custom-control-label' for="sikap_tubuh_5">Cacat</label>
+											</div>
+										</div>
+									</div>
+			                    </div>
+							</div>
+							<div class="form-group col-lg-12">
+								 <div class="col-12 col-md-12 mb-3">
+									<label for="cara_masuk">Cara Masuk :</label>
+									<div class="row col-md-12" id="cara_masuk">
+										<div class="col-md-3">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="cara_masuk" value="cara_masuk_1" id="cara_masuk_1">
+												<label class='custom-control-label' for="cara_masuk_1">Jalan</label>
+											</div>
+										</div>
+										<div class="col-md-3">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="cara_masuk" value="cara_masuk_2" id="cara_masuk_2">
+												<label class='custom-control-label' for="cara_masuk_2">Kursi Roda</label>
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="cara_masuk" value="cara_masuk_3" id="cara_masuk_3">
+												<label class='custom-control-label' for="cara_masuk_3">Lainnya:</label>
+												<input type="text" class="form-control" name="cara_masuk_lainnya">
+											</div>
+										</div>
+									</div>
+			                    </div>
+							</div>
+							<div class="form-group col-lg-12">
+								 <div class="col-12 col-md-12 mb-3">
+									<label for="rujukan">Rujukan :</label>
+									<div class="row col-md-12" id="rujukan">
+										<div class="col-md-3">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="rujukan" value="rujukan_1" id="rujukan_1">
+												<label class='custom-control-label' for="rujukan_1">Tidak</label>
+											</div>
+										</div>
+										<div class="col-md-3">
+											<div class="custom-control custom-radio">
+												<input type="radio" class="custom-control-input" name="rujukan" value="rujukank_2" id="rujukan_2">
+												<label class='custom-control-label' for="rujukan_2">Ya</label>
+											</div>
+										</div>
+										
+									</div>
+			                    </div>
+							</div>
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Nadi</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Nadi">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>x/menit</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Suhu</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Suhu">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span><sup>o</sup>C</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Pernafasan</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Pernafasan">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>x/menit</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Berat Badan</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Berat Badan">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>kg</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Tinggi Badan</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Tinggi Badan">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>cm</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group col-lg-8">
+								<label for="txt_tekanan_darah">Lingkar Lengan Atas</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Lingkar Lengan Atas">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>cm</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row" style="margin-top: 20px;">
+	<div class="col-lg">
+		<div class="card">
+			<div class="card-header card-header-large bg-white d-flex align-items-center">
+				<h5 class="card-header__title flex m-0">Objective</h5>
+			</div>
+			<div class="card-body">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="row">
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Tekanan Darah</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Tekanan Darah">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>mmHg</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Nadi</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Nadi">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>x/menit</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Suhu</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Suhu">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span><sup>o</sup>C</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Pernafasan</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Pernafasan">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>x/menit</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Berat Badan</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Berat Badan">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>kg</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group col-lg-6">
+								<label for="txt_tekanan_darah">Tinggi Badan</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Tinggi Badan">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>cm</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="form-group col-lg-8">
+								<label for="txt_tekanan_darah">Lingkar Lengan Atas</label>
+								<div class="input-group input-group-merge">
+									<input type="text" id="txt_tekanan_darah" class="form-control form-control-appended" required="" placeholder="Lingkar Lengan Atas">
+									<div class="input-group-append">
+										<div class="input-group-text">
+											<span>cm</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						Pemeriksaan Fisik
+						<div id="txt_pemeriksaan_fisik"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg">
+		<div class="card">
+			<div class="card-header card-header-large bg-white d-flex align-items-center">
+				<h5 class="card-header__title flex m-0">Assesment</h5>
+			</div>
+			<div class="card-header card-header-tabs-basic nav" role="tablist">
+				<a href="#asesmen-kerja" class="active" data-toggle="tab" role="tab" aria-controls="asesmen-kerja" aria-selected="true">Diagnosa Kerja</a>
+				<a href="#asesmen-banding" data-toggle="tab" role="tab" aria-selected="false">Diagnosa Banding</a>
+			</div>
+			<div class="card-body tab-content">
+				<div class="tab-pane active show fade" id="asesmen-kerja">
+					<div class="form-group col-lg-12">
+						<label for="txt_tekanan_darah">ICD 10</label>
+						<div class="input-group input-group-merge">
+							<select id="txt_icd_10_kerja" class="form-control"></select>
+						</div>
+						<br />
+						<div id="txt_diagnosa_kerja"></div>
+					</div>
+				</div>
+				<div class="tab-pane show fade" id="asesmen-banding">
+					<div class="form-group col-lg-12">
+						<label for="txt_tekanan_darah">ICD 10</label>
+						<div class="input-group input-group-merge">
+							<select id="txt_icd_10_banding" class="form-control"></select>
+						</div>
+						<br />
+						<div id="txt_diagnosa_banding"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
