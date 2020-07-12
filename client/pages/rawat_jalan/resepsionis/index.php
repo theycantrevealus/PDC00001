73dd79bcb9a8f5fd5e1a4b807a1c804c9dab5ca8
Hendry Tanaka <tanaka@pondokcoder.com>
2020-07-12 13:11:19 +0700

    Susunan resepsionis

--- client/pages/rawat_jalan/resepsionis/index.php
+++ client/pages/rawat_jalan/resepsionis/index.php
@@ -1,40 +1,66 @@
+<style type="text/css">
+	.tabelScroll {
+		height: 500px;
+		overflow-x: scroll;
+		overflow: scroll;
+	}
+</style>
+
 <div class="container-fluid page__heading-container">
 	<div class="page__heading d-flex align-items-center">
 		<div class="flex">
 			<nav aria-label="breadcrumb">
 				<ol class="breadcrumb mb-0">
 					<li class="breadcrumb-item"><a href="<?php echo __HOSTNAME__; ?>/">Home</a></li>
-					<li class="breadcrumb-item"><a href="<?php echo __HOSTNAME__; ?>/master/inventori">Master Inventori</a></li>
-					<li class="breadcrumb-item active" aria-current="page">Satuan</li>
+					<li class="breadcrumb-item active" aria-current="page">Antrian</li>
 				</ol>
 			</nav>
+			<h4 class="m-0">Antrian</h4>
 		</div>
-		<button class="btn btn-sm btn-info" id="tambah-kunjungan">
+		<button class="btn btn-info btn-sm ml-3" id="btnTambahAntrian">
 			<i class="fa fa-plus"></i> Tambah
 		</button>
 	</div>
 </div>
+
 <div class="container-fluid page__container">
-	<div class="row card-group-row">
-		<div class="col-lg-12 col-md-12 card-group-row__col">
-			<div class="card card-group-row__card card-body card-body-x-lg flex-row align-items-center">
-				<table class="table table-bordered" id="table-kunjungan">
-					<thead>
-						<tr>
-							<th style="width: 20px;">No</th>
-							<th>Waktu</th>
-							<th>Kategori</th>
-							<th>No. RM</th>
-							<th>Nama</th>
-							<th>Dokter</th>
-							<th>Keterangan</th>
-							<th>Oleh</th>
-							<th>Aksi</th>
-						</tr>
-					</thead>
-					<tbody></tbody>
-				</table>
-			</div>
-		</div>
-	</div>
+    <div class="row">
+        <div class="col-lg">
+            <div class="card">
+            	<div class="card-body">
+            		<!-- <div class="col-md-12 row float-center">
+						<div class="col-md-2">
+							<label>Departemen <span class="red"></span></label>
+						</div>
+						<div class="col-md-4">
+							<select class="form-control" id="select_departemen">
+								<option value="">Semua</option>
+							</select>
+						</div>
+	                </div> -->
+	                <hr />
+					<div class="table-responsive border-bottom">
+						<table class="table table-bordered table-striped" id="table-antrian-rawat-jalan" style="font-size: 0.9rem;">
+							<thead>
+								<tr>
+									<th width="2%">No</th>
+									<th>Waktu Masuk</th>
+									<th>No. RM</th>
+									<th>Pasien</th>
+									<th>Departemen</th>
+									<th>Dokter</th>
+									<th>Penjamin</th>
+									<th>Oleh</th>
+									<!-- <th>Aksi</th> -->
+								</tr>
+							</thead>
+							<tbody>
+								
+							</tbody>
+						</table>
+					</div>
+            	</div>
+            </div>
+        </div>
+    </div>
 </div>
