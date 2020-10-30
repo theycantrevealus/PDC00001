70d8d236dd50f984c9af45059d414e150306271d
TANAKA <tanaka@pondokcoder.com>
2020-10-30 10:59:51 +0700

    Migration Feature Init

---
+++ api/app/Migration.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PondokCoder;
+
+use PondokCoder\Query as Query;
+use PondokCoder\QueryException as QueryException;
+use PondokCoder\Utility as Utility;
+
+
+class Migration extends Utility
+{
+    static $pdo;
+    static $query;
+
+    protected static function getConn()
+    {
+        return self::$pdo;
+    }
+
+    public function __construct($connection)
+    {
+        self::$pdo = $connection;
+        self::$query = new Query(self::$pdo);
+    }
+
+    private function check_current_table() {
+        $tables = self::$query->select('information_schema', array())
+            ->execute();
+        return $tables;
+
+
+
+    }
+}
+
+?>
