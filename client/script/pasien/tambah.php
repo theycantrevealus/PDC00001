4ba658c8a7c1fde4ae06390b02947df4e44da07a
Hendry Tanaka <44671357+theycantrevealus@users.noreply.github.com>
2021-11-16 11:49:11 +0700

    Merged Pasien

--- client/script/pasien/tambah.php
+++ client/script/pasien/tambah.php
@@ -118,7 +118,9 @@
                     },
                     type: "POST",
                     success: function(response) {
-                        if (status_antrian === "true"){ 		//redirect to tambah kunjungan
+                        console.clear();
+                        console.log(response);
+                        if (status_antrian === "true") { 		//redirect to tambah kunjungan
 							if (
 							    response.response_package.response_unique !== "" &&
                                 response.response_package.response_unique !== undefined &&
@@ -127,7 +129,9 @@
 								//Set Current Pasien dan Antrian Data
 								localStorage.setItem("currentPasien", response.response_package.response_unique);
 								//Notif loket lain yang sedang aktif pemanggilan yang sama?? Back Log??
-								location.href = __HOSTNAME__ + '/rawat_jalan/resepsionis/tambah/' + response.response_package.response_unique;
+                                if(response.response_package.response_unique !== undefined) {
+                                    location.href = __HOSTNAME__ + '/rawat_jalan/resepsionis/tambah/' + response.response_package.response_unique;
+                                }
 							}
 						} else {
 						    if(response.response_package.response_result > 0) {
