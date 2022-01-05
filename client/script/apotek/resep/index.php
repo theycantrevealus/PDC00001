cb070cb0359d52a68d46fdad351363bfefcaec9f
Hendry Tanaka <44671357+theycantrevealus@users.noreply.github.com>
2022-01-05 11:26:09 +0700

    Trace Data Pasien

--- client/script/apotek/resep/index.php
+++ client/script/apotek/resep/index.php
@@ -230,8 +230,11 @@
                 },
                 {
                     "data" : null, render: function(data, type, row, meta) {
-                        console.log(row);
-                        return ((row.pasien_info.panggilan_name !== undefined && row.pasien_info.panggilan_name !== null) ? row.pasien_info.panggilan_name.nama : "") + " " + row.pasien_info.nama;
+                        if(row.pasien_info !== undefined && row.pasien_info !== null) {
+                            return ((row.pasien_info.panggilan_name !== undefined && row.pasien_info.panggilan_name !== null) ? row.pasien_info.panggilan_name.nama : "") + " " + row.pasien_info.nama;
+                        } else {
+                            return row.pasien_info.nama;
+                        }
                     }
                 },
                 {
