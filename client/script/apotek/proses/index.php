7f0ae81c92f6628584f7142fd81546bd0cb236af
Hendry Tanaka <44671357+theycantrevealus@users.noreply.github.com>
2022-01-10 18:11:15 +0700

    NS Check

--- client/script/apotek/proses/index.php
+++ client/script/apotek/proses/index.php
@@ -398,7 +398,13 @@
                     "data" : null, render: function(data, type, row, meta) {
                         if(row.departemen !== null && row.departemen !== undefined) {
                             if (row.departemen.uid === __POLI_INAP__) {
-                                return row.departemen.nama + "<br />" + "<span class=\"text-info\">[" + row.ns_detail.kode_ns + "]</span>" + row.ns_detail.nama_ns;
+                                if(row.ns_detail !== undefined && row.ns_detail !== null) {
+                                    return row.departemen.nama + "<br />" + "<span class=\"text-info\">[" + row.ns_detail.kode_ns + "]</span>" + row.ns_detail.nama_ns;
+                                } else {
+                                    console.log(row.ns_detail);
+                                    return "";
+                                }
+                                
                             } else {
                                 return row.departemen.nama;
                             }
