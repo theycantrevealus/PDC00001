b7261031a9587923a4676ef9da2b10028edd5751
Teguh <teguh.rem@gmail.com>
2022-11-15 20:59:36 +0700

    #610 #614 Fix View File

--- client/script/igdv2/dokter/asesmen-detail.php
+++ client/script/igdv2/dokter/asesmen-detail.php
@@ -448,6 +448,62 @@
         });
 
 
+        $("body").on("click", ".lampiran_view_trigger", function() {
+            var target = $(this).attr("target");
+            $("#modal-lampiran-viewer").modal("show");
+
+            var request = new XMLHttpRequest();
+            request.open('GET', target, true);
+            request.responseType = 'blob';
+            request.onload = function() {
+                var reader = new FileReader();
+                reader.readAsDataURL(request.response);
+                reader.onload =  function(e){
+                    var fileReader = new FileReader();
+                    fileReader.onload = function() {
+                        var pdfData = new Uint8Array(this.result);
+                        // Using DocumentInitParameters object to load binary data.
+                        var loadingTask = pdfjsLib.getDocument({
+                            data: pdfData
+                        });
+                        loadingTask.promise.then(function(pdf) {
+                            // Fetch the first page
+                            var pageNumber = 1;
+                            pdf.getPage(pageNumber).then(function(page) {
+                                var scale = 1.5;
+                                var viewport = page.getViewport({
+                                    scale: scale
+                                });
+                                // Prepare canvas using PDF page dimensions
+                                var canvas = $("#pdfViewer")[0];
+                                var context = canvas.getContext('2d');
+                                canvas.height = viewport.height;
+                                canvas.width = viewport.width;
+                                // Render PDF page into canvas context
+                                var renderContext = {
+                                    canvasContext: context,
+                                    viewport: viewport
+                                };
+                                var renderTask = page.render(renderContext);
+                                renderTask.promise.then(function() {
+                                    //$("#btnSubmit").removeAttr("disabled").html("Terima SK").removeClass("btn-warning").addClass("btn-primary");
+                                });
+                            });
+                        }, function(reason) {
+                            // PDF loading error
+                            console.error(reason);
+                        });
+                    };
+                    //fileReader.readAsArrayBuffer(file);
+                    fileReader.readAsArrayBuffer(request.response);
+                };
+            };
+            request.send();
+
+            return false;
+        });
+
+
 
         $(".print_manager").click(function() {
             var targetSurat = $(this).attr("id");
@@ -601,6 +657,28 @@
         }
     });
 </script>
+
+
+<div id="modal-lampiran-viewer" class="modal fade" role="dialog" aria-labelledby="modal-large-title" aria-hidden="true" data-backdrop="static" data-keyboard="false" style="z-index: 2048;">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-large-title">Lampiran Pemeriksaan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <canvas style="width: 100%; border: solid 1px #ccc;" id="pdfViewer"></canvas>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">
+                    <i class="fa fa-times"></i> Tutup
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <div id="form-inap" class="modal fade" role="dialog" aria-labelledby="modal-large-title" aria-hidden="true" data-backdrop="static" data-keyboard="false">
