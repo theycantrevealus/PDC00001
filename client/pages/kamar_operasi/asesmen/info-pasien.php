47d21181a928a8df9a2ac65648c3ace2f13b7596
Teguh <teguh.rem@gmail.com>
2022-12-14 11:46:56 +0700

    #627

---
+++ client/pages/kamar_operasi/asesmen/info-pasien.php
@@ -0,0 +1,24 @@
+<div class="card card-form">
+		<div class="row no-gutters">
+			<div class="col-lg-12 card-form__body card-body">
+				<div class="form-row">
+					<div class="col-12 col-md-12 mb-3">
+						<label for="pasien">Pasien</label>
+						<input disabled type="text" id="pasien_nama" class="form-control" value="">
+                    </div>
+                    <div class="col-6 col-md-6 mb-3">
+						<label for="no_rm_pasien">No. RM</label>
+						<input disabled type="text" id="no_rm_pasien" class="form-control" value="">
+					</div>
+                    <div class="col-6 col-md-6 mb-3">
+						<label for="nik_pasien">NIK</label>
+						<input disabled type="text" id="nik_pasien" class="form-control" value="">
+					</div>
+                    <div class="col-6 col-md-6 mb-3">
+                        <label for="penjamin">Penjamin</label>
+                        <input disabled type="text" id="penjamin" class="form-control" value="">
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
