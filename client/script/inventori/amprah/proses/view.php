fb0aab39b0bbd01e389a79f88bf6756c782760b5
Hendry Tanaka <theycantrevealus@gmail.com>
2024-03-08 20:00:36 +0700

    Check data for amprah proses

--- client/script/inventori/amprah/proses/view.php
+++ client/script/inventori/amprah/proses/view.php
@@ -255,13 +255,13 @@
 				for(var bKey in metaData[selectedItem].batch) {
 					if(metaData[selectedItem].batch[bKey].gudang.uid == __GUDANG_UTAMA__) {
 						if(metaData[selectedItem].batch[bKey].batch == currentBatch) {
-						    console.log('Batch ditemukan')
+						    console.log('Batch ditemukan');
 							if(metaData[selectedItem].batch[bKey].disetujui == undefined) {
 								metaData[selectedItem].batch[bKey].disetujui = 0;
 							}
 							
 							metaData[selectedItem].batch[bKey].disetujui = parseFloat(currentCount);
-							console.log('Target batch ditemukan')
+							console.log('Target batch ditemukan' + metaData[selectedItem].batch[bKey].disetujui);
 						} else {
 						  console.log('Bukan batch terkini')
 						}
